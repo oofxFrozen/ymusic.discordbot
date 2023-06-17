3ad63512b3711716bdb4f4c93c92441ca6197f69
oofxFrozen <frozenshyper@gmail.com>
2023-06-17 22:20:36 +0300

    Discord and YMusic connection

--- main.py
+++ main.py
@@ -20,24 +20,28 @@ from yandex_music import Client
 import config
 from config import settings
 
+# Initialize bot connection instance with intents
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
-bot = commands.Bot(command_prefix='/', intents=intents)
+bot = commands.Bot(command_prefix='~', intents=intents)
 
+# Initialize YMusic connection instance
 ym_client = Client(config.YM_TOKEN).init()
 
 
 @bot.event
 async def on_ready():
-    await bot.tree.sync(guild=discord.Object(id=config.settings['guild']))
+    """Function that is called on establishing of the connection"""
+    await bot.tree.sync(guild=discord.Object(id=settings['guild']))
     print(f'{bot.user} is ready to serve.')
 
 
 @bot.tree.command(name='hello', description="If you're that lonely to ask a bot to say hello.",
-                  guild=discord.Object(id=config.settings['guild']))
+                  guild=discord.Object(id=settings['guild']))
 async def _hello(interaction):
-    await interaction.response.send_message("Whiskey on rocks, {}?".format(interaction.user))
+    """/hello command"""
+    await interaction.response.send_message("Hello, {}!".format(interaction.user))
 
 
 bot.run(config.settings['token'])
